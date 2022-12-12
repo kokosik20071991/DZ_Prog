25e281183645021154c57ca20c7a87cca4762d0e
Viktoria <vikatim4encko@yandex.ru>
2022-12-12 16:14:11 +0300

    Добавлена задача №6

---
+++ ZD_6/Program.cs
@@ -0,0 +1,11 @@
+﻿int a;
+Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+if (a %2==0)
+{
+    Console.Write("Четное число");
+}
+else
+{
+    Console.Write("Нечентное число");
+}
