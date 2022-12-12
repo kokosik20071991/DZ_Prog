68e5302e08d989a260d107ef1834283c06762356
Viktoria <vikatim4encko@yandex.ru>
2022-12-12 16:02:41 +0300

    Добавлена задача 2

---
+++ ZD_2/Program.cs
@@ -0,0 +1,19 @@
+﻿int a, b;
+int Max=0, Min=0;
+Console.Write("Введите первое число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+Console.Write("Введите второе число: ");
+int.TryParse(Console.ReadLine()!, out b);
+if (a > b)
+{
+    Max=a;
+    Min=b;
+    Console.Write($"Max={a}, Min={b}");
+}
+else
+{
+    Min=a; 
+    Max=b;
+    Console.Write($"Min={a}, Max={b}");
+}
