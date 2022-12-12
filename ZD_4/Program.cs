68d1b6a0d19eae61f7993d63b05a0b9728a05e11
Viktoria <vikatim4encko@yandex.ru>
2022-12-12 16:01:04 +0300

    Добавлена задача 2 и 4

---
+++ ZD_4/Program.cs
@@ -0,0 +1,14 @@
+﻿int a=0, b=0, c=0, Max=0;
+Console.Write("Введите первое число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+Console.Write("Введите второе число: ");
+int.TryParse(Console.ReadLine()!, out b);
+
+Console.Write("Введите третье число: ");
+int.TryParse(Console.ReadLine()!, out c);
+if (a > Max ) Max=a;
+if (b > Max ) Max=b;
+if (c > Max) Max=c;
+Console.Write($"Максимальное из трех чисел: {Max}");
+
