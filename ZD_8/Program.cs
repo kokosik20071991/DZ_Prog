b150bba14414f44ed78f84bba43a5fd442018dc8
Viktoria <vikatim4encko@yandex.ru>
2022-12-12 16:35:42 +0300

    Добавлена задача №8

---
+++ ZD_8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.Write("Введите число:   ");
+string txt = Console.ReadLine();
+int N = Convert.ToInt32(txt);
+int num = 0;
+Console.WriteLine($"Чётные числа из диапазона от 1 до {N}:");
+while (num < N)
+{
+    num++;
+    if (num % 2 == 0) 
+    Console.Write($"{num} ");
+}
+Console.WriteLine(); 
+Console.WriteLine();
